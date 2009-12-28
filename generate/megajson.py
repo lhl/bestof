d3595b3f77002c29e28645fee6e90c18bf1ef797
lhl <lhl@randomfoo.net>
2009-12-28 02:02:12 -0800

    Fixing an artist, also fixing megajson to read/output correctly

--- generate/megajson.py
+++ generate/megajson.py
@@ -5,8 +5,8 @@ import glob, json, sys
 
 bestof = {}
 
-for jsonfile in glob.glob('2*.json'):
-  year = jsonfile[0:4]
+for jsonfile in glob.glob('json/*.json'):
+  year = jsonfile[5:9]
   bestof[year] = json.load(open(jsonfile, 'r'))
 
 json.dump(bestof, open('bestof.json', 'w'))
