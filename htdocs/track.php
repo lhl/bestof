9738765098b86ef3658316b801a52485f7b25ef7
lhl <lhl@randomfoo.net>
2016-08-08 12:32:22 -0400

    updated rate limiting, fixup some unicode issues

--- htdocs/track.php
+++ htdocs/track.php
@@ -19,19 +19,30 @@ if($exit && !$debug) {
   fwrite($f, 'Referer: ' . $_SERVER["HTTP_REFERER"] . "\n\n");
   fclose($f);
   /* */
+  header('HTTP/1.1 403 Forbidden'); 
   exit();
 }
 
 // Check simple play limiter
 // Just to keep the server from melting down...
-$hits_per_second = 5;
-$cache = new Memcache;
-$cache->connect('localhost', 11211) or die;
-$hits = $cache->get('bestof:ratelimiter');
+
+/* TODO:
+  Smarter per client limit tracking...
+  * Does Flash pass SESSION cookies?
+  * use session stamps - limit # of active sessions
+    * can be passed around... - per session limits? (100)
+*/
+$limit = 50;
+$limit_time = 300; // 5 minutes (~1 track)
+
+$cache = new Memcached;
+$cache->addServer('localhost', 11211) or die ("Could not connect");
+
+$hits = $cache->get('bestof.ratelimiter');
 if(!$hits) { $hits = 0; }
-if($hits < $hits_per_second) {
+if($hits < $limit) {
   $hits++;
-  $cache->set('bestof:ratelimiter', $hits, 0, 1);
+  $cache->set('bestof.ratelimiter', $hits, 0, $limit_time);
 } else {
   // More than hits_per_second!
   header('Content-type: text/plain');
@@ -43,6 +54,13 @@ if($hits < $hits_per_second) {
 
   exit;
 }
+
+function shutdown() {
+  $cache->decrement('bestof.ratelimiter');
+}
+register_shutdown_function('shutdown');
+
+
 
 
 
@@ -56,16 +74,10 @@ if(!$trackid = preg_replace('/[^0-9]/', '', $url[3])) {
 
 // Load bestof.json
 // apc: 5ms; memcache: 7ms; file: 12ms;
-if(! $bestof = apc_fetch('bestof.json')) {
-  $cache = new Memcache;
-  $cache->connect('localhost', 11211) or die ("Could not connect to Memcache");
-  if(!$bestof = $cache->get('bestof.json')) {
+if($_SERVER['QUERY_STRING'] == 'nocache') { $nocache = 1; }
+if((!$bestof = $cache->get('bestof.json')) || $nocache) {
+  if((!$bestof = $cache->get('bestof.json')) || $nocache) {
     $bestof = json_decode(file_get_contents('bestof.json'), true);
-
-    // apc
-    apc_store('bestof.json', $bestof);
-
-    // memcache
     $cache->set('bestof.json', $bestof);
   }
 }
@@ -76,6 +88,26 @@ foreach($bestof as $year) {
   foreach($year as $track) {
     if($track['id'] == $trackid) {
       $path = html_entity_decode(urldecode($track['file']));
+
+      // Fixups
+      switch($trackid) {
+        case 27589:
+          // Unicode é (good) vs é (bad)
+          $path = "/locker/music/Yann Tiersen - 2001 - Amelie OST (Le Fabuleux Destin d'Amélie Poulain)/02 - Yann Tiersen - Les jours tristes.mp3";
+          break;
+        case 44605:
+          $path = "/locker/music/Bïa - 2008 - Nocturno/Bia-11 - Madalena.mp3";
+          break;
+        case 44423:
+          $path = "/locker/music/Janelle Monáe - 2007 - Metropolis Suite I - The Chase/Janelle Monáe - Sincerely, Jane.mp3";
+          break;
+        case 39773:
+          $path = "/locker/music/Detektivbyrån - 2006 - Hemvägen EP/03 - Nattöppet.mp3";
+          break;
+        case 39781:
+          $path = "/locker/music/Detektivbyrån - 2006 - Hemvägen EP/07 - Vänerhavet.mp3";
+          break;
+      }
 
       if($debug) {
         print $path . "\n";
